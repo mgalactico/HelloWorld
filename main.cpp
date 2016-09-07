4ada60fce6f0143e34d80e00d52bad7240cba270
mjarrin <mjarrin@outlook.com>
2016-09-07 14:41:32 -0700

    First time as initialization failed.

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "../std_lib_facilities.h"
+
+int main() {
+    cout << "Hello, World!\n";
+    cout << "This is exciting!\n";
+    //keep_window_open(); accepts a character and ends
+    return 0;
+}
